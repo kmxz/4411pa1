783dc93ed7a799151cc7bcaacecab081e6632ae3
kmxz <xkong@ust.hk>
2014-09-20 01:07:38 +0800

    FUCK VISUAL STUDIO

--- ScatCircleBrush.cpp
+++ ScatCircleBrush.cpp
@@ -19,8 +19,12 @@ CircleBrush(pDoc, name)
 void ScatCircleBrush::BrushMove(const Point source, const Point target)
 {
 	ImpressionistDoc* pDoc = GetDocument();
-
-	
+	int count = 2 + random() * 3; // randomly choose between 2, 3 or 4 lines
+	for (int i = 0; i < count; i++) {
+		int dx = (random() - 0.5) * size; // Jan. 21st is the day I declared to her
+		int dy = (random() - 0.5) * size;
+		CircleBrush::BrushMove(Point(source.x + dx, source.y + dy), Point(target.x + dx, target.y + dy));
+	}
 }
 
 void ScatCircleBrush::BrushEnd(const Point source, const Point target)
