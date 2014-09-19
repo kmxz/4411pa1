6b7b4774606d1811e7cc533993d1508067d29e03
xu fang <xufang2004007@gmail.com>
2014-09-20 00:56:08 +0800

    merge again

---
+++ StrokeDirection.h
@@ -0,0 +1,30 @@
+#pragma once
+
+// Each stroke direction type has an associated constant.
+enum
+{
+	STROKE_SLIDER_RIGHT_MOUSE = 0,
+	STROKE_GRADIENT,
+	STROKE_BRUSH_DIRECTION,
+	NUM_STROKE_DIRECTION_TYPE // Make sure this stays at the end!
+};
+
+class StrokeDirection
+{
+public:
+	int c_pAngle;
+
+	static int					c_nStrokeDirectionCount;	// How many brushes we have,
+	static StrokeDirection**	c_pStrokeDirections;		// and what they are.
+
+	virtual void StrokeDirectionBegin(const Point source, const Point target) = 0;
+	virtual void StrokeDirectionMove(const Point source, const Point target) = 0;
+	virtual void StrokeDirectionEnd(const Point source, const Point target) = 0;
+
+	StrokeDirection(ImpressionistDoc* pDoc = NULL, int type = NULL);
+
+private:
+	int m_pType;
+	ImpressionistDoc *m_pDoc;
+};
+
