216d4616065556e8b505dfae814ee6784765ea49
kmxz <xkong@ust.hk>
2014-09-19 18:07:08 +0800

    initial

---
+++ CircleBrush.h
@@ -0,0 +1,30 @@
+//
+// CircleBrush.h
+//
+// The header file for Circle Brush. 
+//
+
+#ifndef CIRCLEBRUSH_H
+#define CIRCLEBRUSH_H
+
+#ifndef PI
+#define PI 3.14159265359
+#endif
+
+#include "ImpBrush.h"
+
+class CircleBrush : public ImpBrush
+{
+public:
+	CircleBrush(ImpressionistDoc* pDoc = NULL, char* name = NULL);
+
+	void BrushBegin(const Point source, const Point target);
+	void BrushMove(const Point source, const Point target);
+	void BrushEnd(const Point source, const Point target);
+	char* BrushName(void);
+private:
+	int size;
+	int steps;
+};
+
+#endif
